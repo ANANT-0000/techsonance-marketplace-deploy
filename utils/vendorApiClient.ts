675e0e032593c7f19afa57fa551a35bdf1338e18
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2026-05-05 15:11:36 +0530

    '.'

--- utils/vendorApiClient.ts
+++ utils/vendorApiClient.ts
@@ -671,3 +671,35 @@ export const fetchGetCompanyRefunds = async (token: string) => {
         throw error;
     }
 };
+export const fetchCompanyCustomers = async (offset: number, limit: number, status: string, sortBy: string, token: string) => {
+    try {
+        const domain = await getCompanyDomain();
+        const response = await fetch(`${BASE_API_URL}/v1/company/customers?offset=${offset}&limit=${limit}&status=${status}&sortBy=${sortBy}`, {
+            cache: 'no-store',
+            // next: { revalidate: 3600 },
+            headers: {
+                'company-domain': domain,
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+export const FetchSuspendCustomer = async (customerId: string, token: string) => {
+    try {
+        const domain = await getCompanyDomain();
+        const response = await fetch(`${BASE_API_URL}/v1/users/${customerId}/suspend`, {
+            method: 'PATCH',
+            headers: {
+                'company-domain': domain,
+                Authorization: `Bearer ${token}`,
+            }
+        });
+ 
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
