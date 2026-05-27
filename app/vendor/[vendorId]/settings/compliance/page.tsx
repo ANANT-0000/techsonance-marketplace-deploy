e43adf7a0ff0ac45d4afb616f0b29b3d8647b50d
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2026-05-27 16:32:28 +0530

    'ds'

--- app/vendor/[vendorId]/settings/compliance/page.tsx
+++ app/vendor/[vendorId]/settings/compliance/page.tsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 
-export const CompliancePage = () => {
+export default function CompliancePage() {
   return (
-    <div>page</div>
+    <div>Compliance Page</div>
   )
 }
